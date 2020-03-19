52968cf04d29d3397b2f5112ad058b4586f622f8
Frolova Olga <Olka_frol@mail.ru>
2020-03-19 16:30:48 +0300

    add layout

--- Recipes/app/src/main/java/com/example/Recipes/screens/search.java
+++ Recipes/app/src/main/java/com/example/Recipes/screens/search.java
@@ -1,15 +1,33 @@
 package com.example.Recipes.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.Recipes.R;
 
-public class search extends AppCompatActivity {
-
+public class search extends AppCompatActivity  implements View.OnClickListener{
+    ImageButton recipe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        recipe = (ImageButton) findViewById(R.id.imageButton);
+        recipe.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+
+            case R.id.imageButton:
+                Intent intent = new Intent(search.this, Recipes.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
