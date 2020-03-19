6b167d7a1e474590d7ebac8749fc6b73c53bd48c
Frolova Olga <Olka_frol@mail.ru>
2020-03-19 17:05:52 +0300

    add layout

---
+++ Recipes/app/src/main/java/com/example/Recipes/screens/Recipes.java
@@ -0,0 +1,23 @@
+package com.example.Recipes.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.Recipes.R;
+
+public class Recipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+
+
+    }
+
+
+}
